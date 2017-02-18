47e5d371d58ccf2f81ee74fe6ee5cc7742e45d5d
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-18 02:48:32 -0500

    Interface

---
+++ MoodTracker WatchKit App/tableRowController.swift
@@ -0,0 +1,15 @@
+//
+//  tableRowController.swift
+//  MoodTracker
+//
+//  Created by Deborah on 2/18/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class tableRowController: NSObject {
+
+    @IBOutlet var tableRowLabel: WKInterfaceLabel!
+}
